1ab4ee43c04806e9be352d59c6e24c6e1a4023c1
Michael Gillett <51103663+migillett@users.noreply.github.com>
2025-01-30 22:13:48 -0500

    Update __main__.py

--- meshtastic_listener/__main__.py
+++ meshtastic_listener/__main__.py
@@ -148,21 +148,6 @@ class MeshtasticListener:
     def __handle_position__(self, packet: dict) -> None:
         logging.debug(f"Received position packet: {packet}")
         position = packet.get('decoded', {}).get('position', {})
-        
-        """{
-            "latitudeI": 341049344,
-            "longitudeI": -844890112,
-            "altitude": 297,
-            "time": 1738256931,
-            "locationSource": "LOC_EXTERNAL",
-            "groundSpeed": 0,
-            "groundTrack": 0,
-            "precisionBits": 13,
-            "raw": "latitude_i: 341049344\nlongitude_i: -844890112\naltitude: 297\ntime: 1738256931\nlocation_source: LOC_EXTERNAL\nground_speed: 0\nground_track: 0\nprecision_bits: 13\n",
-            "latitude": 34.1049344,
-            "longitude": -84.4890112
-        }"""
-        
         self.db.upsert_position(
             node_num=packet['from'],
             last_heard=position.get('time'),
