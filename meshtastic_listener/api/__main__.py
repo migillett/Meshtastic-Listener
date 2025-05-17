7f793a2ba748c01791e4857bd39e18e0b38c6b9a
Michael Gillett <51103663+migillett@users.noreply.github.com>
2025-05-16 23:21:24 -0400

    Update __main__.py

--- meshtastic_listener/api/__main__.py
+++ meshtastic_listener/api/__main__.py
@@ -39,4 +39,4 @@ app.mount("/static", StaticFiles(directory=path.join(BASE_DIR, "static")), name=
 
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
