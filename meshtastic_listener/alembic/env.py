b3fe246f9666e61e5e0232d79beabeba6bbceb66
Michael Gillett <51103663+migillett@users.noreply.github.com>
2025-05-24 13:57:13 -0400

    initialize alembic db

---
+++ meshtastic_listener/alembic/env.py
@@ -0,0 +1,84 @@
+from logging.config import fileConfig
+from os import environ
+
+from meshtastic_listener.listener_db.db_tables import Base
+
+from sqlalchemy import create_engine
+
+from alembic import context
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = Base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def get_server_url() -> str:
+    username = environ.get("POSTGRES_USER", 'postgres')
+    password = environ.get("POSTGRES_PASSWORD")
+    hostname = environ.get("POSTGRES_HOSTNAME", "listener_db")
+    db_name = environ.get("POSTGRES_DATABASE", 'listener_db')
+    port = 5432
+    return f'postgresql://{username}:{password}@{hostname}:{port}/{db_name}'
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    context.configure(
+        url=get_server_url(),
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = create_engine(get_server_url())
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
