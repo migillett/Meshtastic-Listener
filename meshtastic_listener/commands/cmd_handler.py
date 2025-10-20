ab1047c4cfd13a72cda00c4a010dc238ec8eb30a
Michael Gillett <51103663+migillett@users.noreply.github.com>
2025-10-20 14:24:34 -0400

    alert symbols

--- meshtastic_listener/commands/cmd_handler.py
+++ meshtastic_listener/commands/cmd_handler.py
@@ -60,7 +60,12 @@ class CommandHandler:
         if len(links) == 0:
             return 'No links found'
 
-        return '\n'.join([f'{link.nodeNum} ({link.longName}): {link.hostSoftwareVersion}' for link in links])
+        return '\n'.join(
+            [
+                f'{link.nodeNum} ({link.longName}): {link.hostSoftwareVersion} {"⚠️" if link.reconnectAttempts > 0 else "☑️"}'
+                for link in links
+            ]
+        )
 
     # def cmd_subscriptions(self, context: MessageReceived) -> str:
     #     '''
