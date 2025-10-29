ef2ecd60f919404ee547843d61ffa14e514c548d
Michael Gillett <51103663+migillett@users.noreply.github.com>
2025-10-29 13:37:26 -0400

    Update __main__.py

--- meshtastic_listener/__main__.py
+++ meshtastic_listener/__main__.py
@@ -303,7 +303,6 @@ class MeshtasticListener:
                         destinationId=target.nodeNum,
                         portNum=PortNum.TRACEROUTE_APP,
                         wantResponse=True,
-                        # onResponse=self.interface.onResponseTraceRoute,
                         channelIndex=0,
                         hopLimit=self.max_hops,
                     )
